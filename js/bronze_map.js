3f7f26b7d023ced38f57047b4efdd4a00a0dd567
lex berman <vajlex@users.noreply.github.com>
2018-08-08 18:26:03 -0400

    remove redundant js
    files from archive

--- js/bronze_map.js
+++ js/bronze_map.js
@@ -1,42 +1,24 @@
 /*
  * Custom configurations for Museum Feasting maps.
- * Lex Berman, 2017-2018
+ * Lex Berman, 2017-2018, www.dbr.nu
  */
 
-
  //adding base layers
-    var streets = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiYWxleGZvcnR1bmEiLCJhIjoiZmJjNzA5YzVkZTkzZjU5MzRjNTA4NDkxNjI2MGJlYjgifQ.ZxI0M3tinJFDw7F5oQwseQ', {
-        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap<\/a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA<\/a>, Imagery © <a href="http://mapbox.com">Mapbox<\/a>',
-        maxZoom: 18, opacity: 0.6,
-        id: 'alexfortuna.mn58gf63',
-        accessToken: 'pk.eyJ1IjoiYWxleGZvcnR1bmEiLCJhIjoiZmJjNzA5YzVkZTkzZjU5MzRjNTA4NDkxNjI2MGJlYjgifQ.ZxI0M3tinJFDw7F5oQwseQ'}),
-
-        carto_light = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
-            attribution: 'code &raquo; <a href="http://www.dbr.nu/bio" target="_new">Lex Berman</a>',
-            maxZoom: 19, opacity: 0.8}),
+    var 
+        esri = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Shaded_Relief/MapServer/tile/{z}/{y}/{x}', {
+        attribution: 'Tiles &copy; ESRI.  &nbsp;  Map design: <a href="https://www.dbr.nu" target="_blank">Lex Berman</a>',
+        maxZoom: 16, opacity: 0.8}),
 
         natGeo = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}', {
         attribution: 'Tiles &copy; Esri &mdash; National Geographic, Esri, DeLorme, NAVTEQ, UNEP-WCMC, USGS, NASA, ESA, METI, NRCAN, GEBCO, NOAA, iPC',
-        maxZoom: 16, opacity: 0.6});
+        maxZoom: 16, opacity: 0.6})
+    ;  
 
-        thunder = L.tileLayer('https://tile.thunderforest.com/cycle/{z}/{x}/{y}{r}.png?apikey=b06874d83797418fa918de875a67aeb8', {
-        attribution: 'Tiles &copy; Thunderforest & Gravitystorm Ltd.',
-        maxZoom: 16, opacity: 0.8});
-
-        esri = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Shaded_Relief/MapServer/tile/{z}/{y}/{x}', {
-        attribution: 'Tiles &copy; ESRI.',
-        maxZoom: 16, opacity: 0.8});
-        
-
-    // base maps for control
+ // base layer select options
     var basemaps = {
         "Terrain": esri, 
- //       "Gray": carto_light,
         "Modern Map": natGeo,
- //       "Terrain (OSM)": streets,
- //       "Terrain (Thunderforest)": thunder
     };
-
 
 //  conventions
 //  raw data variables abbreviated in lower case:  rgn1, rgn2, art1
@@ -44,7 +26,7 @@
 
 
 // get BRONZE layer data
-var art_bronze = 'https://raw.githubusercontent.com/vajlex/feasting/master/data/art_bronze_20180618.geojson';
+var art_bronze = 'https://raw.githubusercontent.com/vajlex/feasting/master/data/art_bronze_20180808_redo.geojson';
 var city_bronze = 'https://raw.githubusercontent.com/vajlex/feasting/master/data/city_bronze_20180430.geojson';
 var region_bronze = 'https://raw.githubusercontent.com/vajlex/feasting/master/data/region_bronze_20171219.geojson';
 
@@ -61,8 +43,6 @@ function regionB(feature, layer) {
   layer.bindTooltip(label, {permanent: true, direction: "center", className: "label-2"}).openTooltip();
 }
 
-
-
 function cityLabelB(feature, layer) {
   layer.bindTooltip(label, {permanent: true, direction: "center", className: "label-3", offset: [0,20]}).openTooltip();
 }
@@ -71,15 +51,18 @@ function onEachFeature(feature, layer) {
       var check1 = "";
       if(!feature.properties.Period){check1 += ""} 
       else {check1 += "Period: " + feature.properties.Period}
+
       var check2 = "";
       if(!feature.properties.Culture){check2 += ""} 
       else {check2 += "<br />Culture: " + feature.properties.Culture}
+
       var check3 = "";
       if(!feature.properties.Description){check3 += ""} 
       else {check3 += "<hr>" + feature.properties.Description + "<br />"}
+
   layer.bindPopup(
     "<strong>" + feature.properties.Title + "</strong><hr>" + check1 + check2
-    + check3     
+    + check3
     + "<br><img src='thumbnails/" 
     + feature.properties.Image_File + "'>", {maxWidth: "200px"}
     );
@@ -99,14 +82,18 @@ function artBronze(feature, layer) {
       if(!feature.properties.Culture){check2 += ""} 
       else {check2 += "<br />Culture: " + feature.properties.Culture}
       var check3 = "";
-      if(!feature.properties.Description){check3 += ""} 
-      else {check3 += "<hr>" + feature.properties.Description  + "<br />"}
+      if(!feature.properties.Desc_text){check3 += ""} 
+      else {check3 += "<hr>" + feature.properties.Desc_text  + "<br />"}
+      var check4 = "";    
+      if(!feature.properties.Credit){check4 += ""} 
+      else {check4 += "<p><em>Image: " + feature.properties.Credit + "</em>"}
+
   layer.bindPopup(
     "<strong>" + feature.properties.Title + "</strong><hr>" + check1 + check2
-    + check3     
+    + check3
     + "<br><a href='800px/" + feature.properties.Image_File +  "' target='_blank' title='get larger image'><img src='thumbnails/" 
-    + feature.properties.Image_File + "'></a>", {maxWidth: "200px"}
-
+    + feature.properties.Image_File + "'></a>"
+    + check4
     );
 /*  drop mouseover for IOS ipad click to work
     layer.on('mouseover', function(e) {
